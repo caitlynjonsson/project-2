a497271bf7493f8f48cd5b86890470016f10956d
Caitlyn Jonsson <caitlynjonsson@Caitlyns-MacBook-Air.local>
2020-11-26 12:38:26 +0100

    fixing user options

--- content.js
+++ content.js
@@ -1,25 +1,36 @@
-// var elements = document.getElementsByTagName('p');
-//var elements = document.getElementsByTagName('*');
+var elements = document.getElementsByTagName('p');
+var elements = document.getElementsByTagName('*');
 
 
-//for (var i = 0; i < elements.length; i++) {
-//   var element = elements[i];
-//   console.log( element );
-// element.style.setProperty('border','solid 1px red');
+for (var i = 0; i < elements.length; i++) {
+   var element = elements[i];
+   console.log( element );
+element.style.setProperty('border','solid 1px white');
 
-//var r = Math.floor(Math.random()*255);
-//var g = Math.floor(Math.random()*255);
-//var b = Math.floor(Math.random()*255);
-//var c = "rgb(" + r + "," + g + "," + b + ")";
+var r = 255; //Math.floor(Math.random()*255);
+var g = Math.floor(Math.random()*255);
+var b = Math.floor(Math.random()*255);
+var c = "rgb(" + r + "," + g + "," + b + ")";
 
-//element.style.setProperty('background-color',c);
+element.style.setProperty('background-color',c);
 
-//}
+}
+
+chrome.storage.sync.get({fromStored:"", toStored:""}, function(result) {
 
 var html = document.querySelector('html');
 var walker = document.createTreeWalker(html,NodeFilter.SHOW_TEXT);
 
 var node;
 while (node = walker.nextNode()) {
-  node.nodeValue = node.nodeValue.replace(/trump/gi,'Some Guy');
+var re = new RegExp(result.fromStored,"gi");
+node.nodeValue = node.nodeValue.replace(re,result.toStored);
+node.nodeValue = node.nodeValue.replace(/hate/gi,'love');
+node.nodeValue = node.nodeValue.replace(/war/gi,'peace');
+node.nodeValue = node.nodeValue.replace(/ugly/gi,'beautiful');
+node.nodeValue = node.nodeValue.replace(/Donald Trump/gi,'A criminal');
+node.nodeValue = node.nodeValue.replace(/Trump/gi,'criminal');
+node.nodeValue = node.nodeValue.replace(/Donald/gi,'A');
   }
+
+});
